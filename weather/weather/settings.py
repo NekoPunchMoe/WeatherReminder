28e8c6b89c7060082ce0ed5d87ae811faa484693
NekoPunch <wenxin5291311@sina.com>
2017-03-24 21:44:17 -0700

    Update for frontend and user authentication

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -48,7 +48,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reminder',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'django.contrib.sites',
 ]
+
+SITE_ID = 1
+# auth and allauth settings
+LOGIN_REDIRECT_URL = '/'
+ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
+ACCOUNT_EMAIL_REQUIRED = True
+# Make email verification mandatory to avoid junk email accounts
+ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -67,6 +79,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
            os.path.join(BASE_DIR, 'templates', 'reminder'),
+           os.path.join(BASE_DIR, 'templates', 'allauth'),
            ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -79,6 +92,13 @@ TEMPLATES = [
         },
     },
 ]
+
+AUTHENTICATION_BACKENDS = (
+    # Needed to login by username in Django admin, regardless of `allauth`
+    "django.contrib.auth.backends.ModelBackend",
+    # `allauth` specific authentication methods, such as login by e-mail
+    "allauth.account.auth_backends.AuthenticationBackend"
+)
 
 WSGI_APPLICATION = 'weather.wsgi.application'
 
--- weather/weather/urls.py
+++ weather/weather/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import url
+from django.conf.urls import include, url
 from django.contrib import admin
 import reminder.views as reminder_views
 
@@ -8,4 +8,5 @@ urlpatterns = [
    url(r'^del_reminder/', reminder_views.del_reminder),
    url(r'^test_email/', reminder_views.test_email),
    url(r'^secret_trigger/', reminder_views.secret_trigger),
+   url(r'^accounts/', include('allauth.urls')),
 ]
